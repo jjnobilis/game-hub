fcc2c03b8efab337ae97cc6c771ff3868e991cf6
Nobilis <jgnangnelasme@gmail.com>
2023-09-29 13:41:01 +0000

    Refactor Color Mode Switch

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -1,17 +1,23 @@
-import { HStack, Switch, Text, useColorMode } from "@chakra-ui/react";
+import { Button, HStack, useColorMode } from "@chakra-ui/react";
+import { BsMoonFill, BsSun } from "react-icons/bs";
 
 const ColorModeSwitch = () => {
   const { toggleColorMode, colorMode } = useColorMode();
   return (
     <HStack>
-      <Switch
+      <Button
+        as={Button}
+        children={colorMode === "dark" ? <BsMoonFill /> : <BsSun />}
+        onClick={toggleColorMode}
+      ></Button>
+      {/* <Switch
         colorScheme="green"
         isChecked={colorMode === "dark"}
         onChange={toggleColorMode}
       ></Switch>
       <Text whiteSpace="nowrap">
         {colorMode === "dark" ? "Mode sombre" : "Mode Clair"}
-      </Text>
+      </Text> */}
     </HStack>
   );
 };
